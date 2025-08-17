4610edec4c12afcf654a05a6146a74fdd82591b6
chukwuka <davidchukwuka.fab@gmail.com>
2025-08-17 20:31:50 +0100

    cha

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 export default function Home() {
 
   return (
-  <main className="min-h-dvh bg-[url(/chef.jpg)] bg-no-repeat bg-center bg-cover bg-fixed">
-    <section className=" min-h-dvh bg-black/40 flex items-center justify-center">
+  <main>
+    {/* hero section */}
+    <section className="min-h-dvh bg-[url(/chef.jpg)] bg-no-repeat bg-center bg-cover bg-fixed">
+    <div className=" min-h-dvh bg-black/40 flex items-center justify-center">
     <div className="lg:max-w-4xl max-lg:p-3 space-y-10">
       <h1 className="text-white text-center lg:text-6xl font-extrabold text-4xl">
         Welcome to <span className="text-yellow-600 font-semibold ">Tasty Slides</span></h1>
@@ -22,6 +24,53 @@ export default function Home() {
         hover:bg-gray-200 transition-colors duration-300 text-center">Our Story</Link>
       </div> */}
     </div>
+    </div>
+    </section>
+
+    {/* new section */}
+    <section className="space-y-2">
+   <div className="items-center justify-center bg-white p-5 space-y-3">
+    <h2 className="lg:text-3xl text-1xl font-bold text-black">Popular Foods!</h2>
+
+    <div className="grid lg:grid-cols-4 grid-cols-2 gap-3 w-full items-center justify-center">
+    <div className="relative cursor-pointer space-y-2">
+      <div> <img src="jollof.jpg" alt="food" className="rounded-sm object-contain w-full"/></div>
+      <div>
+        <h2 className="lg:text-2xl text-l font-medium text-center justify-center">Jollof Rice</h2>
+      <p className="lg:text-base text-xs px-2 text-justify">A West African classic, is a one-pot wonder that's easy
+        to make and packed with spicy goodness. Get ready for a flavor explosion!
+      </p>
+    </div>
+    </div>
+    <div className="relative cursor-pointer space-y-2">
+      <div> <img src="afang.jpeg" alt="food" className="rounded-sm object-contain w-full"/></div>
+      <div>
+        <h2 className="lg:text-2xl text-l font-medium text-center justify-center">Afang Soup</h2>
+      <p className="lg:text-base text-xs px-2 text-justify">
+       Nigeria's deliciously rich & spicy stew made with afang leaves, meat or fish, and a blend of flavorful spices.
+       A true West African culinary delight! </p>
+    </div>
+    </div>
+    <div className="relative cursor-pointer space-y-2">
+      <div> <img src="egusi.jpg" alt="food" className="rounded-sm object-contain w-full"/></div>
+      <div>
+        <h2 className="lg:text-2xl text-l font-medium text-center justify-center">Egusi Soup</h2>
+      <p className="lg:text-base text-xs px-2 text-justify">
+        A Nigerian favorite made with ground melon seeds, vegetables, and your choice of protein.
+        Rich, flavorful, and nourishing - a West African classic!</p>
+    </div>
+    </div>
+    <div className="relative cursor-pointer space-y-2">
+      <div> <img src="yam.jpg" alt="food" className="rounded-sm object-contain w-full"/></div>
+      <div>
+        <h2 className="lg:text-2xl text-l font-medium text-center justify-center">Yam and Egg</h2>
+      <p className="lg:text-base text-xs px-2 text-justify">
+        A match made in heaven! Boiled or fried yam paired with scrambled or fried eggs - a simple satisfying Nigerian combo.
+      </p>
+    </div>
+    </div>
+    </div>
+  </div>
     </section>
 
   </main>
